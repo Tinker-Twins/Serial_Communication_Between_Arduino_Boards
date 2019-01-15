e8aba0d79bbf47b918bab222d23c7330f35e8b18
Samak Twins <samaktwins@gmail.com>
2019-01-15 14:41:38 +0530

    Add files via upload

---
+++ Receiver.ino
@@ -0,0 +1,12 @@
+int recievedData;
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  while (Serial.available())
+  {
+  recievedData = Serial.read();
+  Serial.println(recievedData);
+  }
+}
